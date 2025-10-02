6e46a7ad90567a55d364400a76d12b0770f1a18c
Mohan Raj <mohanmwraj@gmail.com>
2025-10-02 12:14:21 +0530

    CP

---
+++ Level 1/Week3/C_Even_Odd_Positive_and_Negative.cpp
@@ -0,0 +1,24 @@
+//**********************************************************************************
+// pbm : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/C
+//**********************************************************************************
+#include<bits/stdc++.h>
+#include<iostream>
+using ll = long long;
+using ld = long double;
+using namespace std;
+#define endl "\n";
+#define ff first
+#define ss second
+
+void solve(){
+    
+}
+
+int main() {
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
