d52a5459a0f298eae1370c74e02b85044bb7b71d
Mohan Raj <mohanmwraj@gmail.com>
2025-10-02 12:20:29 +0530

    CP

--- Level 1/Week3/C_Even_Odd_Positive_and_Negative.cpp
+++ Level 1/Week3/C_Even_Odd_Positive_and_Negative.cpp
@@ -11,12 +11,28 @@ using namespace std;
 #define ss second
 
 void solve(){
-    
+    int n; cin >> n;
+
+    int odd = 0, even = 0, pos = 0, neg = 0;
+    for(int i = 0; i < n; ++i){
+        int val; cin >> val;
+        
+        if(val % 2 == 0) even++;
+        else odd++;
+        
+        if(val < 0) neg++;
+        else if(val > 0) pos++;
+    }
+
+    cout<< "Even: " << even << endl;
+    cout<< "Odd: " << odd << endl;
+    cout<< "Positive: "<< pos << endl;
+    cout<< "Negative: "<< neg << endl;
 }
 
 int main() {
     int T = 1;
-    cin >> T;
+    //cin >> T;
     while(T--){
         solve();
     }
