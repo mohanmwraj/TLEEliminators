662c89fed20cc9ee84c6a917a3f0970ddd190d55
Mohan Raj <mohanmwraj@gmail.com>
2025-10-03 11:46:37 +0530

    CP

---
+++ Level 1/Week4/B_Discord.cpp
@@ -0,0 +1,25 @@
+//**********************************************************************************
+// pbm : https://atcoder.jp/contests/abc303/tasks/abc303_b
+//**********************************************************************************
+#include<bits/stdc++.h>
+
+#include<iostream>
+using ll = long long;
+using ld = long double;
+using namespace std;
+#define endl "\n";
+#define ff first
+#define ss second
+
+void solve(){
+    
+}
+
+int main() {
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
---
+++ Level 1/Week4/B_Mark_the_Dust_Sweeper.cpp
@@ -0,0 +1,25 @@
+//**********************************************************************************
+// pbm : https://codeforces.com/problemset/problem/1705/B
+//**********************************************************************************
+#include<bits/stdc++.h>
+
+#include<iostream>
+using ll = long long;
+using ld = long double;
+using namespace std;
+#define endl "\n";
+#define ff first
+#define ss second
+
+void solve(){
+    
+}
+
+int main() {
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
