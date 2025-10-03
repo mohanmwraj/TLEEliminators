66e4a4106796321bc242531e2eaafd2ef3f8d23f
Mohan Raj <mohanmwraj@gmail.com>
2025-10-03 17:17:05 +0530

    CP

--- Level 1/Week3/pattern2.cpp
+++ Level 1/Week3/pattern2.cpp
@@ -12,12 +12,35 @@ using namespace std;
 #define ss second
 
 void solve(){
-    
+    int n; cin >> n;
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n - i - 1; ++j){
+                cout << " ";
+            }
+            
+            for(int j = 0; j < i + 1; ++j){
+                cout << "* ";
+            }
+            
+            cout << endl;
+        }
+        
+        for(int i = n - 1; i >= 0; --i){
+            for(int j = 0; j < n - i - 1; ++j){
+                cout << " ";
+            }
+            
+            for(int j = 0; j < i + 1; ++j){
+                cout << "* ";
+            }
+            
+            cout << endl;
+        }    
 }
 
 int main() {
     int T = 1;
-    cin >> T;
+   // cin >> T;
     while(T--){
         solve();
     }
