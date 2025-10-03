b5c45a4d3ce524afff6cb87ff7451279cb8423a2
Mohan Raj <mohanmwraj@gmail.com>
2025-10-03 13:01:55 +0530

    CP

--- Level 1/Week3/H_One_Prime.cpp
+++ Level 1/Week3/H_One_Prime.cpp
@@ -2,22 +2,35 @@
 // pbm : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/H
 //**********************************************************************************
 #include<bits/stdc++.h>
-
 #include<iostream>
+using namespace std;
 using ll = long long;
 using ld = long double;
-using namespace std;
 #define endl "\n";
 #define ff first
 #define ss second
 
 void solve(){
-    
+    int n; cin >> n;
+    if(n <= 1) {
+        cout << "NO" << endl;
+        return;
+    }
+
+    for(int i = 2; i < n; ++i){
+        if(n % i == 0){
+            cout << "NO" << endl;
+            return;
+        }
+    }
+
+    cout << "YES" << endl;
+
 }
 
 int main() {
     int T = 1;
-    cin >> T;
+    //cin >> T;
     while(T--){
         solve();
     }
--- Level 1/Week3/pattern.cpp
+++ Level 1/Week3/pattern.cpp
@@ -12,12 +12,26 @@ using namespace std;
 #define ss second
 
 void solve(){
-    
+    int n; cin >> n;
+
+    for(int i = 0; i < n; ++i) cout<<'*';
+    cout<<endl;
+
+    for(int i = 0; i < n - 2; ++i){
+        int noSpaces = (n - 2) - i;
+        for(int j = 0; j < noSpaces; ++j)
+            cout << " ";
+        
+        cout << '*' << endl;
+    }
+
+    for(int i = 0; i < n; ++i) cout<<'*';
+    cout<<endl;
 }
 
 int main() {
     int T = 1;
-    cin >> T;
+    //cin >> T;
     while(T--){
         solve();
     }
