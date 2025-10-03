732a1a5c8af29410ed43403bcd8110e302691659
Mohan Raj <mohanmwraj@gmail.com>
2025-10-03 11:39:44 +0530

    CP

---
+++ Level 1/Week3/patternPrinting.cpp
@@ -0,0 +1,25 @@
+//**********************************************************************************
+// pbm : https://www.geeksforgeeks.org/problems/pattern-printing1347/1
+//**********************************************************************************
+#include<bits/stdc++.h>
+
+#include<iostream>
+using ll = long long;
+using ld = long double;
+using namespace std;
+#define endl "\n";
+#define ff first
+#define ss second
+
+void solve(){
+    
+}
+
+int main() {
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
