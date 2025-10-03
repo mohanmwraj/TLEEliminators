5310ae66141040e3381f9b766e64af763cc3c24d
Mohan Raj <mohanmwraj@gmail.com>
2025-10-03 16:45:36 +0530

    CP

--- Level 1/Week3/R_Permutation_with_arrays.cpp
+++ Level 1/Week3/R_Permutation_with_arrays.cpp
@@ -12,12 +12,29 @@ using namespace std;
 #define ss second
 
 void solve(){
-    
+    int n; cin >> n;
+
+    int a[n], b[n];
+    for(int i = 0; i < n; ++i) cin >> a[i];
+    for(int i = 0; i < n; ++i) cin >> b[i];
+
+    sort(a, a+n);
+    sort(b, b+n);
+
+    for(int i = 0; i < n; ++i){
+        if(a[i] != b[i]){
+            cout << "no" << endl;
+            return;
+        }
+    }
+
+    cout << "yes" << endl;
+
 }
 
 int main() {
     int T = 1;
-    cin >> T;
+    //cin >> T;
     while(T--){
         solve();
     }
