56335fa8ca6a1c4b76b1ac31aec9d1b6cbd77beb
Mohan Raj <mohanmwraj@gmail.com>
2025-10-03 18:00:31 +0530

    CP

--- Level 1/Week3/B_Even_Array.cpp
+++ Level 1/Week3/B_Even_Array.cpp
@@ -12,7 +12,31 @@ using namespace std;
 #define ss second
 
 void solve(){
-    
+    int n; cin >> n;
+
+    int a[n];
+    int ei = 0, oi = 0, ee = 0, oe = 0, nm = 0;
+
+    for(int i = 0; i < n; ++i){
+        cin >> a[i];
+
+        if(i % 2 == 0) ei++;
+        else oi++;
+
+        if(a[i] % 2 == 0) ee++;
+        else oe++;
+
+        if(i % 2 != a[i] % 2) nm++;
+    } 
+
+    if(ei != ee || oi != oe) {
+        cout << -1 << endl;
+        return;
+    }
+
+
+    cout << nm / 2 << endl;
+
 }
 
 int main() {
