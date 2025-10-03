92f4af056b2a8b68e39d1957dde395698482cdc3
Mohan Raj <mohanmwraj@gmail.com>
2025-10-03 16:55:46 +0530

    CP

--- Level 1/Week3/patternPrinting.cpp
+++ Level 1/Week3/patternPrinting.cpp
@@ -12,7 +12,15 @@ using namespace std;
 #define ss second
 
 void solve(){
-    
+        int n; cin >> n;
+        
+        for(int i = 1; i <= n; ++i){
+            for(int j = 0; j < i; ++j){
+                cout <<'*';
+            }
+            cout << ' ';
+        }
+        //cout << endl;
 }
 
 int main() {
@@ -23,3 +31,13 @@ int main() {
     }
     return 0;
 }
+/*
+Input:
+N = 5
+Output:
+* ** *** **** ***** 
+Explanation:
+First, print 1 asterisk then space after
+that print 2 asterisk and space and do this
+3 more times.
+*/
