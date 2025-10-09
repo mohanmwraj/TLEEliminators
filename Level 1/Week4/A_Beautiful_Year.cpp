2cb4193c3038d33445f17d0a704f1b8f75db2057
Mohan Raj <mohanmwraj@gmail.com>
2025-10-09 10:15:28 +0530

    CP

--- Level 1/Week4/A_Beautiful_Year.cpp
+++ Level 1/Week4/A_Beautiful_Year.cpp
@@ -12,12 +12,55 @@ using namespace std;
 #define ss second
 
 void solve(){
-    
+    int n; cin >> n;
+
+    // for(int i = n + 1; i < 10000; ++i){
+    //     int freq[10];
+
+    //     for(int j = 0; j < 10; ++j){
+    //         freq[j] = 0;
+    //     }
+    //     //memset(freq, 0, sizeof(freq));
+
+    //     int year = i;
+    //     while(year > 0){
+    //         int digit = year % 10;
+    //         freq[digit]++;
+    //         year /= 10;
+    //     }
+    //     int flag = 0;
+    //     for(int j = 0; j < 10; ++j){
+    //         if(freq[j] > 1) {
+    //             flag = 1;
+    //             break;
+    //         }
+    //     }
+
+    //     if(flag == 0){
+    //         cout << i << endl;
+    //         break;
+    //     }
+    // }
+
+    int a, b, c, d;
+    while(true){
+        n++;
+        a = (n % 10);
+        b = (n / 10) % 10;
+        c = (n / 100) % 10;
+        d = (n / 1000) % 10;
+
+        if(a != b && a != c && a != d && b != c && b != d && c != d){
+            break;
+        }
+    }
+
+    cout << n << endl;
 }
 
 int main() {
     int T = 1;
-    cin >> T;
+   // cin >> T;
     while(T--){
         solve();
     }
