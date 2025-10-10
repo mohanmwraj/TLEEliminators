1e29722ac186c8efeebbedc274d731a106dd28b5
Mohan Raj <mohanmwraj@gmail.com>
2025-10-10 10:02:03 +0530

    CP

--- Level 1/Week4/B_Fill_the_Gaps.cpp
+++ Level 1/Week4/B_Fill_the_Gaps.cpp
@@ -12,12 +12,34 @@ using namespace std;
 #define ss second
 
 void solve(){
-    
+    int n; cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; ++i) cin >> arr[i];
+
+    for(int i = 0; i < n - 1; ++i){
+        cout << arr[i] << " ";
+        if(arr[i] > arr[i + 1]){
+            int temp = arr[i] - 1;
+
+            while(temp > arr[i + 1]){
+                cout << temp << " ";
+                temp--;
+            }
+        } else {
+            int temp = arr[i] + 1;
+            while(temp < arr[i + 1]){
+                cout << temp << " ";
+                temp++;
+            }
+        }
+    }
+    cout << arr[n - 1] << endl;
 }
 
 int main() {
     int T = 1;
-    cin >> T;
+    //cin >> T;
     while(T--){
         solve();
     }
